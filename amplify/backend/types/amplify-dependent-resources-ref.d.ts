b322ed9709eb8373ae805033d375f0e5222ea207
wjmmk <wjmmk3@gmail.com>
2022-03-18 17:47:59 -0500

    Se agregó Almacenamiento y finalizacion de la practica

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "awsamplybackend": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "s35ee46202": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
